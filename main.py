5c91a306523b3368eb933d4d052f3150e6c0ed51
Josh Gray <joshgray91@gmail.com>
2025-03-28 09:11:19 +0000

    Add README and main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+"""All logic will live in here for now."""
+
+def main() -> None:
+    """Main function where app logic is run."""
+    print("hello world")
+
+# ===================EXECTUION STARTS HERE===================
+if __name__ == "__main__":
+    main()
