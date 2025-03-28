28ad3badae48c2a6945d2c4ad3a4377a40a8bd84
Josh Gray <joshgray91@gmail.com>
2025-03-28 10:18:46 +0000

    implement logic to request response from plex server

--- main.py
+++ main.py
@@ -10,14 +10,27 @@ def print_title(line_length: int = 70) -> None:
     print(f"{'-' * title_spacing}{title}{'-' * title_spacing}")
     print(f"{'=' * line_length}")
 
-def validate_url(token: str) -> str:
+def get_plex_response(url: str, token: str) -> requests:
     """calls a plex server with a given token and prints the response."""
-    postcodes_req = requests.get("https://api.postcodes.io/postcodes/SK30DL")
+    token = token.strip()
 
+    try:
+        r = requests.get(f"{url}{token.strip()}")
+        r.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print(f"\n ERROR - please check token \'{token}\' is correct.\n")
+        raise SystemExit(err) from err
+    else:
+        print("\nSuccess! Listing media libraries below.\n")
+        return r
 
 def main() -> None:
     """Main function where app logic is run."""
     print_title()
+
+    token = input("\nPlease enter your plex token: ")
+    print(get_plex_response("http://localhost:32400/library/sections?X-Plex-Token=", token).content)
+
 
 # ===================EXECTUION STARTS HERE===================
 if __name__ == "__main__":
